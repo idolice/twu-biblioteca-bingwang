4969cbcdae4d8ddba878b2216a122d112db6b2a0
idolice <349031587@qq.com>
2017-02-22 17:37:01 +0800

    [Bing]finish this exercise and refactor

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -2,6 +2,7 @@ package com.twu.biblioteca;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by bingwang on 2/21/17.
@@ -9,42 +10,81 @@ import java.util.List;
 public class Library {
     private List<Book> bookList;
     private int bookNumber;
+
     public Library() {
+        this.bookList = init();
+        this.bookNumber = bookList.size();
+    }
+
+    private List<Book> init() {
         List books = new ArrayList();
-        Book book = new Book("advanced mathematics",1);
-        Book book1 = new Book("Harry Porter",1);
-        Book book2 = new Book("Peter Pan",1);
+        Book book = new Book("advanced mathematics", 1);
+        book.setDescription("this is a book to teach you basic knowledge about math");
+        Book book1 = new Book("Harry Porter", 1);
+        book1.setDescription("this book tell a magic story");
+        Book book2 = new Book("Peter Pan", 1);
+        book2.setDescription("Peter Pan never grow up");
         books.add(book);
         books.add(book1);
         books.add(book2);
-        this.bookList = books;
-        this.bookNumber = bookList.size();
+
+        return books;
     }
 
     public String welcome() {
         return "welcome to our library";
     }
-    public int getQuatity(){
-        return bookNumber;
-    }
 
-    public List<Book> getBookList() {
-        return bookList;
+    public int getBookList() {
+        int i;
+        for (i = 0; i < bookList.size(); i++) {
+            System.out.print("option "+i+": "+bookList.get(i).bookName+"\n");
+        }
+        System.out.print("option "+i+": quit\n");
+        Scanner scanner = new Scanner(System.in);
+        int option = scanner.nextInt();
+        if(option>=0&&option<bookNumber){
+            return 1;
+        }
+        if(option == bookNumber){
+            return 0;
+        }
+        return 2;
     }
 
     public int lend(int bookId) {
-        if(bookList.get(bookId).number>0&&bookId<= bookNumber){
+        if (bookId>=0&&bookId <= bookNumber&&bookList.get(bookId).number > 0) {
             bookList.get(bookId).number--;
+            System.out.print("Thank you! Enjoy the book\n");
             return 1;
         }
+        System.out.print("That book is not available\n");
         return 0;
     }
 
     public int recover(int bookId) {
-        if(bookId<= bookNumber){
+        if (bookId>=0&&bookId < bookNumber) {
             bookList.get(bookId).number++;
+            System.out.print("Thank you for returning the book.");
             return 1;
         }
+        System.out.print("That is not a valid book to return.");
         return 0;
+    }
+
+    public int getBookInformation(int bookId) {
+
+        if (bookId>=0&&bookId < bookNumber) {
+            System.out.print(bookList.get(bookId).description+"\n");
+            System.out.print("option 1:checkout book\noption 2:return book\noption 3:quit\n");
+            return 1;
+        }
+        System.out.print("we don't have this book");
+        return 0;
+    }
+
+
+    public void mainMenu() {
+        System.out.print("Main Menu:\noption 1:Book list\noption 2:quit\n");
     }
 }
--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -3,25 +3,43 @@ package com.twu.biblioteca;
 
 import org.junit.Test;
 
-import java.util.List;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertEquals;
 
 /**
  * Created by bingwang on 2/21/17.
  */
 public class LibraryTest {
     Library library = new Library();
+    int result;
     @Test
-    public void should_return_welcome_message(){
-        String welcomeMessage = library.welcome();
-        assertEquals(welcomeMessage,"welcome to our library");
+    public void should_return_1_when_lend_book_is_avialable(){
+        result = library.lend(1);
+        assertThat(result,is(1));
     }
     @Test
-    public void should_return_the_booklist(){
-        List<Book> books = library.getBookList();
-        assertThat(books.get(0).bookName,is("advanced mathematics"));
+    public void should_return_0_when_borrow_invalid_book(){
+        result = library.lend(10);
+        assertThat(result,is(0));
+    }
+    @Test
+    public void should_return_1_when_return_book_successfully(){
+        result = library.recover(0);
+        assertThat(result,is(1));
+    }
+    @Test
+    public void should_return_0_when_return_invalid_book(){
+        result = library.recover(10);
+        assertThat(result,is(0));
+    }
+    @Test
+    public void should_return_1_when_get_valid_book_detail(){
+        result = library.getBookInformation(1);
+        assertThat(result,is(1));
+    }
+    @Test
+    public void should_return_0_when_get_invalid_book_detail(){
+        result = library.getBookInformation(10);
+        assertThat(result,is(0));
     }
 }
