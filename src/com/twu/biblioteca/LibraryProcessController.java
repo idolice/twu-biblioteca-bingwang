27e5d432fc4272e4c386aa6ce4e73483c5eee660
idolice <349031587@qq.com>
2017-03-06 22:25:11 +0800

    [Bing]add a map

--- src/com/twu/biblioteca/LibraryProcessController.java
+++ src/com/twu/biblioteca/LibraryProcessController.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -12,11 +13,22 @@ public class LibraryProcessController {
     private LibraryPrinter libraryPrinter;
     private Library library;
     private CustomerInfo currentCustomer;
+    private Map<Integer,Integer> mainPageChoiceMap;
 
     public LibraryProcessController(LibraryPrinter libraryPrinter, Library library) {
         this.libraryPrinter = libraryPrinter;
         this.library = library;
         this.currentCustomer = new CustomerInfo(Stage.LOGIN_PAGE,null);
+        this.mainPageChoiceMap = initMainPageChoiceMap();
+    }
+
+    private Map<Integer, Integer> initMainPageChoiceMap() {
+        Map<Integer,Integer> tempMap = new HashMap<Integer, Integer>();
+        tempMap.put(Stage.BOOK_LIST_PAGE,1);
+        tempMap.put(Stage.MOVIE_LIST_PAGE,2);
+        tempMap.put(Stage.USER_INFO_PAGE,3);
+        tempMap.put(Stage.EXIT_STAGE,0);
+        return tempMap;
     }
 
     public void process() {
@@ -28,7 +40,6 @@ public class LibraryProcessController {
         while (currentCustomer.getStage()>Stage.LOGIN_PAGE) {
             if(currentCustomer.getStage()==Stage.HOME_PAGE) {
                 libraryPrinter.printMainList();
-
             }
         }
 
